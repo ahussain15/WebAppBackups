0b144c2d1ec4744583323c4c94c0e46a7050b255
ahussain15 <67604653+ahussain15@users.noreply.github.com>
2021-04-19 10:56:10 -0400

    Add files via upload

---
+++ 4_15_21/public/routes/index.js
@@ -0,0 +1,14 @@
+var dogcatfish = require('./dogcatfish.js');
+var facts = require('./facts.js');
+var apod = require('./apod.js');
+var weather = require('./weather.js');
+var cookie_session = require('./cookie_session.js');
+
+module.exports.do_setup = function(app) {
+    dogcatfish.run_setup(app);
+    facts.run_setup(app);
+    apod.run_setup(app);
+    weather.run_setup(app);
+    cookie_session.run_setup(app);
+};
+
